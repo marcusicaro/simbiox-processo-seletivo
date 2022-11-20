633b89f8450f3af477b0686ae16eb72a19fe5d04
Marcus <marcusicaromc@gmail.com>
2022-11-19 22:31:45 -0300

    Create page containing movie overview when clicked

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const searchURL = BASE_URL + '/search/movie?' + API_KEY + '&query=';
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const searchBar = document.getElementById('search');
+const logo = document.getElementById('logo');
 
+// get movies info from TMDB url
 function getMovies(url) {
     fetch(url).then(response => response.json()).then(data => {
         console.log(data.results);
@@ -14,12 +16,11 @@ function getMovies(url) {
     });
 };
 
-getMovies(API_URL);
-
+// display movies from list
 function displayMovies (data){
     main.innerHTML = '';
     data.forEach(movie => {
-        const {title, poster_path, vote_average, overview} = movie;
+        const {title, poster_path, vote_average, overview, id} = movie;
         const movieCard = document.createElement('div');
         movieCard.classList = 'movie-card';
         movieCard.innerHTML = `
@@ -32,14 +33,40 @@ function displayMovies (data){
           <span class="${getScoreColor(vote_average)}">${vote_average}</span>
         </div>
 
-        <div class="overview">
-          <p>${overview}</p>
+        <div class="movie-id">
+        <p>ID: ${id}</p>
         </div>
         `
         main.appendChild(movieCard);
+        
+        // return a page containing only the clicked movie
+        movieCard.addEventListener('click', (e) => {
+            e.preventDefault();
+            main.innerHTML = '';
+            movieCard.innerHTML = `
+            <img src="${IMG_URL + poster_path}" alt="${title}" srcset="">
+            
+            <div class="movie-info"></div>
+          
+            <div class="movie-info">
+              <h3>${title}</h3>
+              <span class="${getScoreColor(vote_average)}">${vote_average}</span>
+            </div>
+    
+            <div class="overview">
+              <p>${overview}</p>
+            </div>
+    
+            <div class="movie-id">
+            <p>ID: ${id}</p>
+            </div>
+            `
+            main.appendChild(movieCard);
+        });
     })
-}
+};
 
+// change the color of the score text of each movie
 function getScoreColor(score) {
     if (score > 8) {
         return 'green';
@@ -47,8 +74,9 @@ function getScoreColor(score) {
         return 'orange';
     } 
     return 'red';
-}
+};
 
+// search based on movies titles
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     
@@ -57,4 +85,11 @@ form.addEventListener('submit', (e) => {
     if(searchTerm){
         return getMovies(searchURL + searchTerm);
     } getMovies(API_URL);
-})
+});
+
+// return to the homepage
+logo.addEventListener('click', () => {
+    getMovies(API_URL);
+});
+
+getMovies(API_URL);
